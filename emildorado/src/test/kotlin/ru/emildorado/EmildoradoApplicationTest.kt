c5060ede72829ee2f2f7dac8364492aaea788c49
amukhametzyanov <amuhametzyanov@vtb.ru>
2021-11-10 21:09:54 +0300

    fix test

--- emildorado/src/test/kotlin/ru/emildorado/EmildoradoApplicationTest.kt
+++ emildorado/src/test/kotlin/ru/emildorado/EmildoradoApplicationTest.kt
@@ -1,10 +1,10 @@
-package com.example.demo
+package ru.emildorado
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class EmildoradoApplicationTest {
 
     @Test
     fun contextLoads() {
